6b0d92e444aff60270d7d33d65bc650b11a1da38
Marcel Friedrich <waynearne1337@googlemail.com>
2022-11-17 18:21:00 +0100

    Extend UI to show case DNN / CNN

--- src/ApplicationUi.py
+++ src/ApplicationUi.py
@@ -11,6 +11,7 @@ from Shape import Shapes
 from Painter import Painter
 from ImagePreprocesser import ImagePreprocesser
 from dnn.DeepNeuralNetwork import load_mnist_model
+from cnn.ConvNeuralNetwork import load_mnist_cnn_model
 
 # Load the UI File
 GUI_MODEL = os.path.join(GUI_DIR, "GUI.ui")
@@ -31,31 +32,61 @@ class ApplicationUi(BASE, FORM):
         self.PaintPanel.close()
         self.DrawingFrame.insertWidget(0, self.PaintPanel)
         self.DrawingFrame.setCurrentWidget(self.PaintPanel)
+        self.default_image_path = self.getImageByName(-1)
 
-        # Set up Label for on hold picture
-        self.label = QtWidgets.QLabel(self)
-        self.label.setGeometry(QtCore.QRect(460, 70, 280, 280))
-        default_image_path = os.path.join(IMAGE_DIR, str(-1) + ".png")
-        self.pixmap = QtGui.QPixmap(default_image_path)
-        self.label.setPixmap(self.pixmap)
+        self.preparePredictionMapForDNN()
+        self.preparePredictionMapForCNN()
 
-        self.Clear_Button.clicked.connect(self.ClearSlate)
-        self.Predict_Button.clicked.connect(self.PredictNumber)
-        self.model = load_mnist_model()
+        self.Clear_Button.clicked.connect(self.clear)
+        self.Predict_Button.clicked.connect(self.predict)
+        self.dnn_model = load_mnist_model()
+        self.cnn_model = load_mnist_cnn_model()
 
-    def ClearSlate(self):
+    def preparePredictionMapForDNN(self):
+        self.headline_dnn = QtWidgets.QLabel(self)
+        self.headline_dnn.setText("Prediction DNN")
+        self.headline_dnn.setGeometry(QtCore.QRect(460, 40, 280, 30))
+
+        self.label_dnn = QtWidgets.QLabel(self)
+        self.label_dnn.setGeometry(QtCore.QRect(460, 70, 280, 280))
+
+        self.pixmap_dnn = QtGui.QPixmap(self.default_image_path)
+        self.label_dnn.setPixmap(self.pixmap_dnn)
+
+    def preparePredictionMapForCNN(self):
+        self.headline_cnn = QtWidgets.QLabel(self)
+        self.headline_cnn.setText("Prediction CNN")
+        self.headline_cnn.setGeometry(QtCore.QRect(780, 40, 280, 30))
+
+        self.label_cnn = QtWidgets.QLabel(self)
+        self.label_cnn.setGeometry(QtCore.QRect(780, 70, 280, 280))
+
+        self.pixmap_cnn = QtGui.QPixmap(self.default_image_path)
+        self.label_cnn.setPixmap(self.pixmap_cnn)
+
+    def clear(self):
         self.DrawingShapes = Shapes()
         self.PaintPanel.repaint()  # type: ignore
-        default_image_path = os.path.join(IMAGE_DIR, str(-1) + ".png")
-        self.pixmap = QtGui.QPixmap(default_image_path)
-        self.label.setPixmap(self.pixmap)
 
-    def PredictNumber(self):
+        self.pixmap_dnn = QtGui.QPixmap(self.default_image_path)
+        self.label_dnn.setPixmap(self.pixmap_dnn)
+
+        self.pixmap_cnn = QtGui.QPixmap(self.default_image_path)
+        self.label_cnn.setPixmap(self.pixmap_cnn)
+
+    def predict(self):
         image_preprocesser = ImagePreprocesser(self.DrawingFrame.grab())
         image = image_preprocesser.process()
-        y_pred_class_idx = self.model.nn_predict(image)
-        image_file_path = os.path.join(
-            IMAGE_DIR, str(y_pred_class_idx) + ".png"
-        )
-        self.pixmap = QtGui.QPixmap(image_file_path)
-        self.label.setPixmap(self.pixmap)
+
+        # Predict dnn
+        self.pixmap_dnn = QtGui.QPixmap(self.getImageByName(self.dnn_model.nn_predict(image)))
+        self.label_dnn.setPixmap(self.pixmap_dnn)
+
+        # Predict cnn
+        self.pixmap_cnn = QtGui.QPixmap(self.getImageByName(self.cnn_model.nn_predict(image)))
+        self.label_cnn.setPixmap(self.pixmap_cnn)
+
+    def getImageByName(self, name: str) -> str:
+        return os.path.join(IMAGE_DIR, str(name) + ".png")
+
+
